0235a9a1a8478e1582fbc99aea2ed7477e08f9e9
marianafrentsos <49728829+marianafrentsos@users.noreply.github.com>
2021-05-26 23:18:24 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import './App.css';
+import Landing from './components/Header/Landing'
+import './variables/variables.scss'
+import React from "react";
+
+function App() {
+  return (
+    <div>
+<Landing/>
+<About/>
+    </div>
+  );
+}
+
+export default App;
